8427535255a0a0e712eb15449774177d6226606a
Jessica Stokes <hello@jessicastokes.net>
2021-08-16 16:58:44 -0700

    Don't force game names to uppercase

--- src/GDMENUCardManager.Core/GdItem.cs
+++ src/GDMENUCardManager.Core/GdItem.cs
@@ -33,7 +33,7 @@ namespace GDMENUCardManager.Core
                 {
                     if (_Name.Length > namemaxlen)
                         _Name = _Name.Substring(0, namemaxlen);
-                    _Name = Helper.RemoveDiacritics(_Name).ToUpperInvariant().Trim();
+                    _Name = Helper.RemoveDiacritics(_Name).Trim();
                 }
 
                 RaisePropertyChanged();
